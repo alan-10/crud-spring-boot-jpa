83d3a3e117c11a2441601d2217e092c3a05bd07c
Alan Diniz <alanzinid@gmail.com>
2024-10-10 15:08:37 -0300

    feat: create service layer and component registration

--- src/main/java/com/crudProject/crud/config/TestConfig.java
+++ src/main/java/com/crudProject/crud/config/TestConfig.java
@@ -20,7 +20,7 @@ public class TestConfig  implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 	
-		User user1 = new User(null, "alan", "alan@gmail.com", "989887t22", "12345");
+		User user1 = new User(null, "alan1", "alan@gmail.com", "989887t22", "12345");
 		User user2 = new User(null, "pablo", "pabloviadinho@gmail.com", "98988733", "12345");
 		
 		userRepository.saveAll(Arrays.asList(user1, user2));
--- src/main/java/com/crudProject/crud/resources/UserResource.java
+++ src/main/java/com/crudProject/crud/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.crudProject.crud.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudProject.crud.entities.User;
+import com.crudProject.crud.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/user")
 public class UserResource {
 
+	@Autowired
+	UserService service;
+
 	@GetMapping("/all")
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "alan", "alan@gmail.com", "099898989", "12334555");
+	public ResponseEntity<List<User>> findAll() {
+
+		List<User> users = service.findAll();
+
+		return ResponseEntity.ok().body(users);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
 
 		return ResponseEntity.ok().body(user);
 	}
